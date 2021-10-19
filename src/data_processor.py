7253e1018a62eb9d07426c82947796129d178caa
Joao Silva <joaofsilva@medinfo1.ieeta.pt>
2021-10-19 15:19:36 +0100

    Debugging

--- src/data_processor.py
+++ src/data_processor.py
@@ -113,8 +113,9 @@ class DataProcessor(object):
     def _read_csv(cls, input_file):
         """Reads a comma separated value file."""
         file=pd.read_csv(input_file)
-        lines=zip(file.SUBJECT_ID,file.HADM_ID,file.ADMITTIME,file.DAYS_NEXT_ADMIT,file.DURATION,file.DIAG_ICD9,\
-                  file.DIAG_CCS,file.PROC_ICD9,file.PROC_CCS,file.NDC,file.Label,file.TEXT)
+        lines=zip(file.HADM_ID,file.Label,file.TEXT)
+        # lines=zip(file.SUBJECT_ID,file.HADM_ID,file.ADMITTIME,file.DAYS_NEXT_ADMIT,file.DURATION,file.DIAG_ICD9,\
+        #           file.DIAG_CCS,file.PROC_ICD9,file.PROC_CCS,file.NDC,file.Label,file.TEXT)
         return lines
 
 class readmissionProcessor(DataProcessor):
@@ -193,8 +194,10 @@ class readmissionProcessor(DataProcessor):
                 features["proc_ccs"] = None
                 features["ndc"] = None
 
-            features["label"] = str(int(line[10]))
-            features["text_a"] = line[11]
+            # features["label"] = str(int(line[10]))
+            # features["text_a"] = line[11]
+            features["label"] = str(int(line[1]))
+            features["text_a"] = line[2]
 
             examples.append(
                 InputExample(guid=guid, features=features, text_b=None))
