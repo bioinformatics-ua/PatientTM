e88dcc55c7360c201a6791807255b1d68cce6528
Joao Silva <joaofsilva@medinfo1.ieeta.pt>
2021-11-19 14:32:34 +0000

    Added embedding precomputation scripts

---
+++ src/data_processor_precomputation.py
@@ -0,0 +1,221 @@
+import os
+import csv
+import json
+import logging
+import pandas as pd
+
+logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt = '%m/%d/%Y %H:%M:%S',
+                    level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+class InputExample(object):
+    """A single training/test example for simple sequence classification."""
+
+    def __init__(self, guid, features, text_b=None):
+        """Constructs a InputExample.
+        Args:
+            guid: Unique id for the example.
+            features: dictionary containing the following features: ADMITTIME,DURATION,DIAG_ICD9,DIAG_CCS,PROC_ICD9,PROC_CCS,NDC,Label,TEXT.
+            TEXT contains the untokenized text of the first sequence. For single sequence tasks, only this sequence must be specified.
+            text_b: (Optional) string. The untokenized text of the second sequence.
+            Only must be specified for sequence pair tasks.
+        """
+        self.guid = guid
+        self.text_a = features["text_a"]
+        self.label = features["label"]
+        self.text_b = text_b
+        self.hadm_id = features["hadm_id"]
+
+
+
+class InputFeatures(object):
+    """A single set of features of data."""
+    def __init__(self, features):
+        self.input_ids = features["input_ids"]
+        self.input_mask = features["input_mask"]
+        self.segment_ids = features["segment_ids"]        
+        self.label_id = features["label_id"]
+        self.hadm_id = features["hadm_id"]    
+
+
+class DataProcessor(object):
+    """Base class for data converters for sequence classification data sets."""
+
+    def get_train_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the train set."""
+        raise NotImplementedError()
+
+    def get_dev_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the dev set."""
+        raise NotImplementedError()
+    
+    def get_test_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the test set."""
+        raise NotImplementedError()
+
+    def get_labels(self):
+        """Gets the list of labels for this data set. This can vary according to the predictive goal in mind (binary, multiclass, regression)"""
+        raise NotImplementedError()
+        
+    @classmethod
+    def _read_csv(cls, input_file):
+        """Reads a comma separated value file."""
+        file=pd.read_csv(input_file)
+        lines=zip(file.SUBJECT_ID,file.HADM_ID,file.Label,file.TEXT)
+        return lines
+
+class readmissionProcessor(DataProcessor):
+    def get_examples(self, file_path, set_type):
+        return self._create_examples(self._read_csv(file_path), set_type)
+    
+    def get_labels(self):
+        """This is only 0 or 1 for readmission prediction. Other predictive goals may need different labels"""
+        return ["0", "1"]
+    
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training, dev and test sets.
+        @param Features is a list with additional variables to be used"""
+        examples = []
+        for (i, line) in enumerate(lines):
+            guid = "%s-%s" % (set_type, i)
+            features = dict()
+            features["hadm_id"] = line[1]
+            features["label"] = str(int(line[2]))
+            features["text_a"] = line[3]
+
+            examples.append(
+                InputExample(guid=guid, features=features, text_b=None))
+        return examples
+
+
+def processString(string, charsToRemove):
+    for char in charsToRemove: string = string.replace(char, "")
+    return string
+
+
+def convertToIds(feature, idsMappingDict):
+    # featureIds=[]
+    # for entry in feature:
+    #     featureIds.append(idsMappingDict[str(entry)])
+    #     print(entry)
+    # return featureIds
+    return [idsMappingDict[str(entry)] for entry in feature]
+
+
+def _truncate_seq_pair(tokens_a, tokens_b, max_length):
+    """Truncates a sequence pair in place to the maximum length."""
+
+    # This is a simple heuristic which will always truncate the longer sequence
+    # one token at a time. This makes more sense than truncating an equal percent
+    # of tokens from each, since if one sequence is very short then each token
+    # that's truncated likely contains more information than a longer sequence.
+    while True:
+        total_length = len(tokens_a) + len(tokens_b)
+        if total_length <= max_length:
+            break
+        if len(tokens_a) > len(tokens_b):
+            tokens_a.pop()
+        else:
+            tokens_b.pop()
+
+def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer):
+    """Loads a data file into a list of `InputBatch`s."""
+
+    label_map = {}
+    for (i, label) in enumerate(label_list):
+        label_map[label] = i
+
+    features = []
+    for (ex_index, example) in enumerate(examples):
+        feature = dict()  
+        tokens_a = tokenizer.tokenize(example.text_a)
+
+        tokens_b = None
+        if example.text_b:
+            tokens_b = tokenizer.tokenize(example.text_b)
+
+        if tokens_b:
+            # Modifies `tokens_a` and `tokens_b` in place so that the total
+            # length is less than the specified length.
+            # Account for [CLS], [SEP], [SEP] with "- 3"
+            _truncate_seq_pair(tokens_a, tokens_b, max_seq_length - 3)
+        else:
+            # Account for [CLS] and [SEP] with "- 2"
+            if len(tokens_a) > max_seq_length - 2:
+                tokens_a = tokens_a[0:(max_seq_length - 2)]
+
+        # The convention in BERT is:
+        # (a) For sequence pairs:
+        #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
+        #  type_ids: 0   0  0    0    0     0       0 0    1  1  1  1   1 1
+        # (b) For single sequences:
+        #  tokens:   [CLS] the dog is hairy . [SEP]
+        #  type_ids: 0   0   0   0  0     0 0
+        #
+        # Where "type_ids" are used to indicate whether this is the first
+        # sequence or the second sequence. The embedding vectors for `type=0` and
+        # `type=1` were learned during pre-training and are added to the wordpiece
+        # embedding vector (and position vector). This is not *strictly* necessary
+        # since the [SEP] token unambigiously separates the sequences, but it makes
+        # it easier for the model to learn the concept of sequences.
+        #
+        # For classification tasks, the first vector (corresponding to [CLS]) is
+        # used as as the "sentence vector". Note that this only makes sense because
+        # the entire model is fine-tuned.
+        tokens = []
+        segment_ids = []
+        tokens.append("[CLS]")
+        segment_ids.append(0)
+        for token in tokens_a:
+            tokens.append(token)
+            segment_ids.append(0)
+        tokens.append("[SEP]")
+        segment_ids.append(0)
+
+        if tokens_b:
+            for token in tokens_b:
+                tokens.append(token)
+                segment_ids.append(1)
+            tokens.append("[SEP]")
+            segment_ids.append(1)
+
+        input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+        # The mask has 1 for real tokens and 0 for padding tokens. Only real
+        # tokens are attended to.
+        input_mask = [1] * len(input_ids)
+
+        # Zero-pad up to the sequence length.
+        while len(input_ids) < max_seq_length:
+            input_ids.append(0)
+            input_mask.append(0)
+            segment_ids.append(0)
+
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(segment_ids) == max_seq_length
+        #print (example.label)
+        label_id = label_map[example.label]
+
+        if ex_index < 2:
+            logger.info("*** Example ***")
+            logger.info("guid: %s" % (example.guid))
+            logger.info("tokens: %s" % " ".join(
+                    [str(x) for x in tokens]))
+            logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+            logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+            logger.info(
+                    "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+            logger.info("label: %s (id = %d)" % (example.label, label_id))
+
+        feature["input_ids"]=input_ids
+        feature["input_mask"]=input_mask
+        feature["segment_ids"]=segment_ids
+        label_id = label_map[example.label]
+        feature["label_id"]=label_id
+        feature["hadm_id"]=example.hadm_id
+
+
+        features.append(InputFeatures(feature))
+    return features
