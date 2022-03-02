a6bda5274a7208edb6e5fa86070d318e71f23276
Joao Silva <joaofsilva@medinfo1.ieeta.pt>
2022-03-02 15:04:30 +0000

    Correcting paths

---
+++ data/extended/preprocessing/subScripts/4-precompute_text_embeddings.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python3 ./cbert_precomputation.py   --task_name readmission   --readmission_mode discharge  --data_dir /PatientTM/data/extended/discharge/   --bert_model /PatientTM/model/discharge_readmission   --max_seq_length 512   --output_dir /PatientTM/results/result_discharge
